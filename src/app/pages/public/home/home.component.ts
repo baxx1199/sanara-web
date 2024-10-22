c8ef2ccca5f201627b511b5c255a7378b2a7ce91
baxx1199 <joshuajulcobaut@gmail.com>
2024-10-21 20:42:24 -0500

    footer

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -3,11 +3,13 @@ import { NavbarComponent } from '../../../shared/navbar/navbar.component';
 import { FeaturedComponent } from '../featured/featured.component';
 import { NgOptimizedImage } from '@angular/common';
 import { AboutUsComponent } from '../about-us/about-us.component';
+import { ProfessionalsComponent } from '../professionals/professionals.component';
+import { FooterComponent } from '../../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports:[NavbarComponent, FeaturedComponent,AboutUsComponent,NgOptimizedImage],
+  imports:[NavbarComponent, FeaturedComponent,AboutUsComponent,ProfessionalsComponent,FooterComponent,NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
--- src/app/pages/public/public.module.ts
+++ src/app/pages/public/public.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { FeaturedComponent } from './featured/featured.component';
+import { ProfessionalsComponent } from './professionals/professionals.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     HomeComponent,
-    FeaturedComponent
+    FeaturedComponent,
+    ProfessionalsComponent
   ],
   declarations: []
 })
